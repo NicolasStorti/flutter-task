c2888441f087cd16ed768a6162d79060f8c0eae5
Nicolas <dev.nicolasstorti@gmail.com>
2023-12-08 14:41:42 -0300

    Correções de espaçamentos dos input

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -8,7 +8,6 @@ class FormScreen extends StatefulWidget {
 }
 
 class _FormScreenState extends State<FormScreen> {
-
   TextEditingController nameController = TextEditingController();
   TextEditingController difficultyController = TextEditingController();
   TextEditingController imageController = TextEditingController();
@@ -21,7 +20,7 @@ class _FormScreenState extends State<FormScreen> {
       key: _formKey,
       child: Scaffold(
         appBar: AppBar(
-          title: Text('Nova Tarefa'),
+          title: const Text('Nova Tarefa'),
         ),
         body: Center(
           child: SingleChildScrollView(
@@ -29,17 +28,18 @@ class _FormScreenState extends State<FormScreen> {
               height: 650,
               width: 400,
               decoration: BoxDecoration(
-                color: Colors.black12,
-                borderRadius: BorderRadius.circular(10),
-                  border: Border.all(width: 3)
-              ),
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(width: 3)),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: TextFormField(
-                      validator: (String? value){
-                        if(value != null && value.isEmpty){
+                      validator: (String? value) {
+                        if (value != null && value.isEmpty) {
                           return 'Insira o nome da Tarefa';
                         }
                         return null;
@@ -49,7 +49,7 @@ class _FormScreenState extends State<FormScreen> {
                       decoration: InputDecoration(
                         border: OutlineInputBorder(),
                         hintText: 'Nome',
-                        fillColor: Colors.white,
+                        fillColor: Colors.white70,
                         filled: true,
                       ),
                     ),
@@ -57,18 +57,21 @@ class _FormScreenState extends State<FormScreen> {
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: TextFormField(
-                      validator: (value){
-                        if(value !.isEmpty || int.parse(value) > 5 || int.parse(value) < 1){
-                          return 'Insira uma Dificultade entre 1 e 5';
+                      validator: (value) {
+                        if (value!.isEmpty ||
+                            int.parse(value) > 5 ||
+                            int.parse(value) < 1) {
+                          return 'Insira uma Dificuldade entre 1 e 5';
                         }
                         return null;
                       },
+                      keyboardType: TextInputType.number,
                       controller: difficultyController,
                       textAlign: TextAlign.center,
                       decoration: InputDecoration(
                         border: OutlineInputBorder(),
                         hintText: 'Dificuldade',
-                        fillColor: Colors.white,
+                        fillColor: Colors.white70,
                         filled: true,
                       ),
                     ),
@@ -76,55 +79,55 @@ class _FormScreenState extends State<FormScreen> {
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: TextFormField(
-                      validator: (value){
-                        if(value!.isEmpty){
-                          return 'Insira uma URL da Imagem!';
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Insira uma URL de Imagem!';
                         }
                         return null;
                       },
-                      onChanged: (text){
-                        setState((){
-
-                        });
+                      keyboardType: TextInputType.url,
+                      onChanged: (text) {
+                        setState(() {});
                       },
                       controller: imageController,
                       textAlign: TextAlign.center,
                       decoration: InputDecoration(
                         border: OutlineInputBorder(),
-                        hintText:'Imagem',
-                        fillColor: Colors.white,
+                        hintText: 'Imagem',
+                        fillColor: Colors.white70,
                         filled: true,
                       ),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      height: 100,
-                      width: 80,
-                      decoration: BoxDecoration(
-                        color: Colors.transparent,
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(width: 2,color: Colors.blue),
-                      ),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: Image.network(imageController.text,
+                  Container(
+                    height: 100,
+                    width: 80,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(width: 2, color: Colors.blue),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(
+                        imageController.text,
                         errorBuilder: (context, error, stackTrace) {
                           return Image.asset('assets/images/nophoto.png');
                         },
                         fit: BoxFit.cover,
-                        ),
                       ),
                     ),
                   ),
-                  ElevatedButton(onPressed: (){
-                    if(_formKey.currentState!.validate()){
-                      print(nameController.text);
-                      print(int.parse(difficultyController.text));
-                      print(imageController.text);
-                    }
-                  }, child: Text('Adicionar'),)
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        print(nameController.text);
+                        print(int.parse(difficultyController.text));
+                        print(imageController.text);
+                      }
+                    },
+                    child: Text('Adicionar!'),
+                  )
                 ],
               ),
             ),
